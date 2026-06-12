1ff30bf8e5f2ccaffa52fb63742976944643166d
Shreya pal <shreyapal7985@gmail.com>
2026-06-12 18:47:24 +0530

    creating sanke.tsx file inside the component folder

--- src/componets/Game.tsx
+++ src/componets/Game.tsx
@@ -1,19 +1,65 @@
 import * as React from 'react'
 
 
-import { StyleSheet,Text } from 'react-native';
+import { StyleSheet,Text,View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {Colors} from '../styles/Colors';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Gesture,GestureDetector } from 'react-native-gesture-handler';
+import { Coordinate, Direction, GestureEventType } from '../types/Type';
+import Snake from './Snake';
+
+const SNAKE_INITAL_POSITION = [{ x: 5, y: 5}];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20};
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax:63};
+const MOVE_INTERVAL =50;
+const SCORE_INCREMENT = 10;
 
  export default function Game():  React.JSX.Element{
-    const handleGsture=(event:any)=>{
-        console.log(event)
+const [direction,setDirection]=React.useState<Direction>(Direction.Right)
+const [snaake, setSnake] = React.useState<Coordinate[]>(SNAKE_INITAL_POSITION)
+
+const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION)
+const [isPaused,setIsPaused]=React.useState<boolean>(false)
+    const pan = Gesture.Pan()
+    .onStart(()=>
+    {
+console.log("finger touches the screen")
+ })
+  .onUpdate((event:GestureEventType) => {
+    console.log("finger moving in X and Y direction");
+    const {translationX, translationY}=event;
+    if(Math.abs(translationX)>Math.abs(translationY)){
+        if(translationX > 0){
+            setDirection(Direction.Right)
+        }
+        else{
+            //moving left
+            setDirection(Direction.Left)
+        }
     }
+    else{
+        if(translationY > 0){
+            //moving down
+            setDirection(Direction.Down)
+        }
+        else{
+            //moving up
+            setDirection(Direction.Up)
+        }
+    }
+    console.log(event)
+  })
+  .onEnd(()=>{
+    console.log("finger leaves the screen")
+  });
     return(
+        <GestureDetector gesture={pan}>
         <SafeAreaView style={styles.container}>
-            
+            <View style={styles.boundaries}>
+                <Snake snake={snaake}/>
+            </View>
         </SafeAreaView>
+        </GestureDetector>
     )
  }
   const styles= StyleSheet.create({
---
+++ src/componets/Snake.tsx
@@ -0,0 +1,36 @@
+import { Fragment } from "react/jsx-runtime";
+import { Coordinate } from "../types/Type";
+import { View, StyleSheet } from "react-native";
+import { Colors } from "../styles/Colors";
+
+
+interface SnakeProps{
+    snakee:Coordinate[];
+}
+
+export default function Snake({snakee}:SnakeProps):React.JSX.Element{
+    return(
+        <Fragment>
+            {snakee.map((segment:Coordinate, index:number)=>{
+                const segmentStyle={
+                    left:segment.x*10,
+                    top: segment.y*10,
+                }
+                return <View key={index} style={[styles.snake,segmentStyle]}/>
+            })}
+        </Fragment>
+    )
+}
+
+const styles=StyleSheet.create(
+    {
+        snake:{
+            width:15,
+            height:15,
+            borderRadius:7
+            backgroundColor:Colors.primary,
+            position:'absolute'
+
+        }
+    }
+)
