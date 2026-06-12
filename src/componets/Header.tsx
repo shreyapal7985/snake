2a9fb6bbceb88c77935d6c822d750bfb79a00c93
Shreya pal <shreyapal7985@gmail.com>
2026-06-12 21:08:42 +0530

    create a Header ,tsx fikle inside the components

---
+++ src/componets/Header.tsx
@@ -0,0 +1,45 @@
+import {TouchableOpacity, View, StyleSheet} from 'react-native';
+import { Colors } from '../styles/Colors';
+import {Ionicons} from '@expo/vector-icons';
+import {FontAwesome} from '@expo/vector-icons';
+
+interface HeaderProps{
+    resetGame:()=>void;
+    pauseGame:()=>void;
+    children: React.JSX.Element;
+    isPaused:boolean;
+}
+export default function Header({
+    children,
+    reloadGame,
+    pauseGame,
+    isPaused,
+}:HeaderProps):React.JSX.Element{
+    return(
+        <View style={styles.container}>
+            <TouchableOpacity onPress={reloadGame}>
+                <Ionicons name='reload-circle' size={35} color={Colors.primary}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={pauseGame}>
+                <FontAwesome name={isPaused?'play-circle':'pause-circle'} 
+                size={35} color={Colors.primary}/>
+            </TouchableOpacity>
+            {children}
+            </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:0.05,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        borderWidth:12,
+        borderColor:Colors.background,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        padding:15
+}
+})
