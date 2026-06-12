613abdfef9ce0e316e3d9865893f7654551d835b
Shreya pal <shreyapal7985@gmail.com>
2026-06-12 19:30:39 +0530

    applied some logic to mave the snake

--- src/componets/Game.tsx
+++ src/componets/Game.tsx
@@ -1,70 +1,100 @@
 import * as React from 'react'
 
 
-import { StyleSheet,Text,View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {Colors} from '../styles/Colors';
-import { Gesture,GestureDetector } from 'react-native-gesture-handler';
+import { Colors } from '../styles/Colors';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import { Coordinate, Direction, GestureEventType } from '../types/Type';
 import Snake from './Snake';
 
-const SNAKE_INITAL_POSITION = [{ x: 5, y: 5}];
-const FOOD_INITIAL_POSITION = { x: 5, y: 20};
-const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax:63};
-const MOVE_INTERVAL =50;
+const SNAKE_INITAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
 const SCORE_INCREMENT = 10;
 
- export default function Game():  React.JSX.Element{
-const [direction,setDirection]=React.useState<Direction>(Direction.Right)
-const [snaake, setSnake] = React.useState<Coordinate[]>(SNAKE_INITAL_POSITION)
+export default function Game(): React.JSX.Element {
+    const [direction, setDirection] = React.useState<Direction>(Direction.Right)
+    const [snaake, setSnake] = React.useState<Coordinate[]>(SNAKE_INITAL_POSITION)
 
-const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION)
-const [isPaused,setIsPaused]=React.useState<boolean>(false)
+    const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION)
+    const [isPaused, setIsPaused] = React.useState<boolean>(false)
+
+    const moveSnake = () => {
+        const snakeHead = snaake[0];
+        const newHead = { ...snakeHead };//creating a copy
+
+        //game over
+
+        switch (direction) {
+            case Direction.Up:
+                newHead.y -= 1;
+                break;
+            case Direction.Down:
+                newHead.y += 1;
+                break;
+            case Direction.Left:
+                newHead.x -= 1;
+                break;
+            case Direction.Right:
+                newHead.x += 1;
+                break;
+
+        }
+    }
     const pan = Gesture.Pan()
-    .onStart(()=>
-    {
-console.log("finger touches the screen")
- })
-  .onUpdate((event:GestureEventType) => {
-    console.log("finger moving in X and Y direction");
-    const {translationX, translationY}=event;
-    if(Math.abs(translationX)>Math.abs(translationY)){
-        if(translationX > 0){
-            setDirection(Direction.Right)
-        }
-        else{
-            //moving left
-            setDirection(Direction.Left)
-        }
-    }
-    else{
-        if(translationY > 0){
-            //moving down
-            setDirection(Direction.Down)
-        }
-        else{
-            //moving up
-            setDirection(Direction.Up)
-        }
-    }
-    console.log(event)
-  })
-  .onEnd(()=>{
-    console.log("finger leaves the screen")
-  });
-    return(
+        .onStart(() => {
+            console.log("finger touches the screen")
+        })
+        .onUpdate((event: GestureEventType) => {
+            console.log("finger moving in X and Y direction");
+            const { translationX, translationY } = event;
+            if (Math.abs(translationX) > Math.abs(translationY)) {
+                if (translationX > 0) {
+                    setDirection(Direction.Right)
+                }
+                else {
+                    //moving left
+                    setDirection(Direction.Left)
+                }
+            }
+            else {
+                if (translationY > 0) {
+                    //moving down
+                    setDirection(Direction.Down)
+                }
+                else {
+                    //moving up
+                    setDirection(Direction.Up)
+                }
+            }
+            console.log(event)
+        })
+        .onEnd(() => {
+            console.log("finger leaves the screen")
+        });
+    return (
         <GestureDetector gesture={pan}>
-        <SafeAreaView style={styles.container}>
-            <View style={styles.boundaries}>
-                <Snake snake={snaake}/>
-            </View>
-        </SafeAreaView>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.boundaries}>
+                    <Snake snakee={snaake} />
+                </View>
+            </SafeAreaView>
         </GestureDetector>
     )
- }
-  const styles= StyleSheet.create({
-container:{
-    backgroundColor:Colors.primary,
-    flex:1
 }
-  })
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.primary,
+        flex: 1
+    },
+    boundaries: {
+        flex: 1,
+        borderColor: Colors.primary,
+        borderWidth: 12,
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+        backgroundColor: Colors.background
+    }
+})
